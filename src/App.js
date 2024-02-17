f27ce7781ea646110c143b850336854b372e335c
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-02-17 09:29:13 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1>Salom</h1>
+          <h1>Salomcha</h1>
           Learn React
         </a>
       </header>
