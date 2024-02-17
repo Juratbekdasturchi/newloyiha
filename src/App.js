4060d38b3d7a8689903a2628e3a9f1278d2b9355
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-02-17 10:02:30 +0500

    fivee committ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-.productCard:hover{
-  box - shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.3);
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-@media screen and(min - width: 1200px) {
-  .productCol {
-    width: 20 % !important;
-  }
-}
-.priceMonth{
-  background - color: red;
-  padding: 1px 8px;
-  border - radius: 20px;
-  color: white;
-  display: inline - block;
-}
-.title{
-  font - size: 18px;
-}
-.productCard.btn{
-  display: block;
-}
-img{
-  width: 300px;
-}
+export default App;
